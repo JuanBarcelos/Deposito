f5db8e69604a121019c109b573c5f5d0f441c4a0
Juan Barcelos <juanbarcelos.ti@outlook.com>
2021-05-05 17:34:40 -0300

    Add routes and StepsHeader

--- frontend/front-web/src/Navbar/index.tsx
+++ frontend/front-web/src/Navbar/index.tsx
@@ -1,11 +1,13 @@
 import './styles.css';
 import {ReactComponent as Logo} from './logo.svg';
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Navbar(){
   return (
     <nav className="main-navbar">
         <Logo />
-        <a href="" className="logo-text"> Deposito Dois Irmãos</a>
+        <Link to="/" className="logo-text"> Deposito Dois Irmãos</Link>
     </nav>
   )
 
---
+++ frontend/front-web/src/Orders/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import StepsHeader from './StepsHeader';
+import './styles.css';
+
+
+function Orders(){
+  return (
+    <StepsHeader/>
+  )
+
+}
+
+
+export default Orders;
