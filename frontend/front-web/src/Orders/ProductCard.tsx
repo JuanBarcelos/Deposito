798787f1c089cbc1abad0225c90ff35ae4216f18
Juan Barcelos <juanbarcelos.ti@outlook.com>
2021-05-07 21:26:52 -0300

    selected product

--- frontend/front-web/src/Orders/ProductCard.tsx
+++ frontend/front-web/src/Orders/ProductCard.tsx
@@ -2,6 +2,8 @@ import { Product } from './Types';
 
 type Props = {
   product: Product;
+  onSelectProduct: (product: Product) => void;
+  isSelected: boolean;
 }
 
 function formatPrice(price:number){
@@ -14,9 +16,11 @@ function formatPrice(price:number){
   return formatter.format(price);
 }
 
-function ProductCard({product}:Props){
+function ProductCard({product, onSelectProduct, isSelected}:Props){
   return (
-    <div className="order-card-container">
+    <div className={`order-card-container ${isSelected ? 'selected' : ''} `} 
+    onClick={()=>onSelectProduct(product)}>
+
         <h3 className="order-card-title"> {product.name}</h3>
         <img src={product.imageUri} className="order-card-image" alt={product.name} />
         <h3 className="order-card-price"> {formatPrice(product.price)}</h3>
